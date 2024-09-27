dadcd8c56a0b505da98df2d0cabc643135962e24
Julio Davila <juliodavila@Julios-MacBook-Pro-2.local>
2024-09-27 12:58:55 -0400

    Adding Leading zeroes

--- SplitCode.js
+++ SplitCode.js
@@ -23,7 +23,10 @@ let generateCode = (ibPersonCode) => {
 
     console.log(`Alpha code: ${alphaIndex}`);
     console.log(`Number code: ${Number(codeNumber)} \n`);
-    document.getElementById("codeEnteredResult").innerHTML = `${message}`;
+    document.getElementById(
+        "codeEnteredResult"
+    ).innerHTML = `AlphaIndex: ${alphaIndex} </br>
+    Code Number: ${codeNumber} </br>  ${message}`;
 };
 
 // Transfer the code entered to a html label.
@@ -34,3 +37,41 @@ let transferCodeToLabel = (personCode) => {
     generateCode(codeEntered.value);
     codeEnteredLabel.innerText = codeEntered.value;
 };
+
+const testingMaps = () => {
+    // Initial map
+    let sequence = new Map();
+    sequence.set(1, { value: "a", nextKey: "2" });
+    console.log(JSON.stringify(sequence.get(1), null, 4));
+
+    // Setting Map
+    let viewMapOnLog = new Map();
+    viewMapOnLog.set("a", "b");
+    viewMapOnLog.set("c", "d");
+    viewMapOnLog.set("d", "e");
+    console.log("viewMapOnLog : ", viewMapOnLog);
+};
+
+//testingMaps();
+
+const generateLeadingZeroes = () => {
+    let numberA = "00009";
+    let numberB = "00001";
+    let totalLength = numberA.length;
+    let total = (Number(numberA) + Number(numberB)).toString();
+    console.log(typeof total);
+    console.log(`Total : ${total}`);
+
+    // Adding leading zeroes
+    total = total.padStart(numberA.length, "0");
+    console.log(`Leading Zeros ${total}`);
+
+    // In APEX you can use leftPad(length, character)
+    /**
+     * String a = '10';
+     * system.debug(a.leftPad(5,'0'));
+     * OUTPUT: 00010
+     */
+};
+
+// generateLeadingZeroes();
