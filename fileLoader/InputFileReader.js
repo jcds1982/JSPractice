38805906d91f452f9e44a35a54c8c4b59e575efc
Julio Davila <juliodavila@Julios-MBP.attlocal.net>
2025-03-30 23:16:28 -0400

    Adding last changes on file reader

--- fileLoader/InputFileReader.js
+++ fileLoader/InputFileReader.js
@@ -3,11 +3,13 @@ let inputFileReader = (id) => {
 
     // Read value if any
     const data = document.getElementById(id);
+
     let fileReader = new FileReader();
+    const fileContent = fileReader.readAsText(data.files[0]);
+
     fileReader.onload = (e) => {
         console.log(e.target.result);
     };
 
-    const fileContent = fileReader.readAsText(data.files[0]);
-    console.log(fileContent);
+    // console.log(fileContent);
 };
